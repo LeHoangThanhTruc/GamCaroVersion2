5f1931357cff89921c1070227fb72d59db3ff763
LeHoangThanhTruc <24521868@gm.uit.edu.vn>
2025-11-29 18:58:18 +0700

    Add files via upload
    chưa fix lỗi hết

---
+++ GameCaroShared/Cons.cs
@@ -0,0 +1,13 @@
+﻿namespace GameCaroShared
+{
+    public class Cons
+    {
+        public static int CHESS_WIDTH = 30;
+        public static int CHESS_HEIGHT = 30;
+        public static int CHESS_BOARD_HEIGHT = 21;
+        public static int CHESS_BOARD_WIDTH = 20;
+        public static int COOL_DOWN_STEP = 100;
+        public static int COOL_DOWN_TIME = 10000; //10s = 10000ms
+        public static int COOL_DOWN_INTERVAL = 100; //100ms = 1/10s
+    }
+}
